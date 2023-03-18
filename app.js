fa119c3302d0c6ec260749f8232ba713a9e392ca
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-03-18 02:04:56 -0300

    Creación de var.entorno con dotenv

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
+require("dotenv").config();
 
 app.use(cors());
 
-const port = 3000;
+// const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-})
+});
