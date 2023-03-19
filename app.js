77703ecfd9f248ac5bdf5d7a891d533f5b624434
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-03-19 02:31:04 -0300

    Conexión con MongoDB

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
+const dbConnect = require("./src/config/mongo");
 
 app.use(cors());
 
@@ -11,3 +12,5 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+
+// dbConnect();
