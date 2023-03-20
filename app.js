9c4d02a19cf1d484846bf1a0394d08d9cce9e99b
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-03-20 01:29:44 -0300

    Creación de ruta principal (#4)
    Creación de path__dirname principal en routes

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.use(cors());
 // const port = 3000;
 const port = process.env.PORT || 3000;
 
+//Route Principal
+app.use("/api", require("./src/routes"));
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
