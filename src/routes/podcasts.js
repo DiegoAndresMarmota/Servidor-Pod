0a2d78a5a08ef6f0eab68be06ff2deb1e86b8540
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-06-10 18:24:31 -0400

    middleware readerParams

--- src/routes/podcasts.js
+++ src/routes/podcasts.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { getItems, getItem, createItem } = require("../controllers/podcasts");
 const { validatorCreateItem } = require("../validators/podcasts");
+const { readerParams } = require("../middlewares/readerParams");
 
 router.get("/", getItems);
 
-router.post("/", validatorCreateItem, createItem);
+router.post("/", validatorCreateItem, readerParams, createItem);
 
 module.exports = router;
