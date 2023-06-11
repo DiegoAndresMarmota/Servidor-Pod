ea6d488272eae1e4cda78de66655859dfaef9451
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-06-10 20:09:50 -0400

    Creación de updatePodcast, deletePodcasts y getPodcast

--- src/controllers/podcasts.js
+++ src/controllers/podcasts.js
@@ -2,59 +2,92 @@ const { matchedData } = require('express-validator');
 const { podcastsModel } = require('../models');
 
 /**
- * Obtener lista de la base de datos
+ * Obtener la lista de podcasts
  * @param {*} req
  * @param {*} res
  */
-const getItems = async (req, res) => {
+const getPodcasts = async (req, res) => {
     try {
         const data = await podcastsModel.find({});
         res.send({ data });
     }catch(error) {
-        handleHttpError(res, "ERROR_NOT_FOUND", 401);
+        handleHttpError(res, "ERROR_NOT_FOUND_ALL_PODCASTS", 401);
     }
 };
 
 
 /**
- * Obtener lista para obtener un item
+ * Obtener el podcast de la lista, segun id
  * @param {*} req
  * @param {*} res
  */
-const getItem = (req, res) => {};
+const getPodcast = async (req, res) => {
+    try {
+        req = matchedData(req);
+        const { id } = req;
+        const data = await podcastsModel.findById(id);
+        res.send({ data });
+    }catch(error) {
+        handleHttpError(res, "ERROR_NOT_FOUND_PODCAST", 404);
+    }
+};
 
 
 /**
- * Crear un item
+ * Crear un podcast en la lista general
  * @param {*} req
  * @param {*} res
  */
-const createItem = async (req, res) => {
+const createPodcast = async (req, res) => {
     try {
         //Validar los datos ingresar en el body
         const validateBody = matchedData(req)
         const data = await podcastsModel.create(validateBody);
         res.send({ data });
     } catch (error) {
-        handleHttpError(res, "ERROR_NOT_CREATE_ITEM", 403);
+        handleHttpError(res, "ERROR_NOT_CREATE_PODCAST", 403);
     }
 };
 
 
 /**
- * Actualizar un item
+ * Actualizar un podcast de la lista general
  * @param {*} req
  * @param {*} res
  */
-const updateItem = (req, res) => { };
+const updatePodcast = async (req, res) => {
+    try {
+        //Validar los datos ingresar en el body
+        const {id, ...validateBody } = matchedData(req);
+        const data = await podcastsModel.findOneAndUpdate(id, validateBody);
+        res.send({ data });
+    } catch (error) {
+        handleHttpError(res, "ERROR_NOT_UPDATE_PODCAST", 402);
+    }
+};
 
 
 /**
- * Eliminar un item
+ * Eliminar un podcast de la lista general
  * @param {*} req
  * @param {*} res
  */
-const deleteItem = (req, res) => {};
+const deletePodcast = async (req, res) => {
+    try {
+        req = matchedData(req);
+        const { id } = req;
+        const data = await podcastsModel.deleteOne({_id:id});
+        res.send({ data });
+    } catch (error) {
+        handleHttpError(res, "ERROR_NOT_DELETE_PODCAST", 404);
+    }
+};
 
 
-module.exports = { getItems, getItem, createItem, updateItem, deleteItem };
+module.exports = {
+    getPodcasts,
+    getPodcast,
+    createPodcast,
+    updatePodcast,
+    deletePodcast,
+};
