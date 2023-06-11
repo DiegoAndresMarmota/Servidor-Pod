a16f02b7d184a6a2515cdf09eedf1ba1e61f326d
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-06-10 20:13:27 -0400

    Features/middleware 02 (#13)
    * Plantilla pendiente
    
    * Creación del handleError
    
    * Implementación del handleHttpError
    
    * validateBody, manejo de Errors en CreateItem
    
    * Creación de validatorGetPodcast
    
    * Creación de routes de Podcast
    
    * Creación de updatePodcast, deletePodcasts y getPodcast

--- src/routes/podcasts.js
+++ src/routes/podcasts.js
@@ -1,11 +1,37 @@
 const express = require("express");
 const router = express.Router();
-const { getItems, getItem, createItem } = require("../controllers/podcasts");
-const { validatorCreateItem } = require("../validators/podcasts");
+const { getPodcasts, getPodcast, createPodcast, updatePodcast, deletePodcast } = require("../controllers/podcasts");
+const { validatorCreatePodcast, validatorGetPodcast} = require("../validators/podcasts");
 // const readerParams = require("../middlewares/readerParams");
 
-router.get("/", getItems);
+/**
+ * Obtener la lista de podcasts
+ */
+router.get("/", getPodcasts);
 
-router.post("/", validatorCreateItem, createItem);
+
+/**
+ * Obtener el podcast de la lista, segun id
+ */
+router.get("/:id", validatorGetPodcast, getPodcast);
+
+
+/**
+ * Crear un podcast en la lista general
+ */
+router.post("/", validatorCreatePodcast, createPodcast);
+
+
+/**
+ * Actualizar un podcast en la lista general
+ */
+router.put("/:id", validatorGetPodcast, validatorCreatePodcast, updatePodcast);
+
+
+/**
+ * Eliminar un podcast de la lista general
+ */
+router.delete("/:id", validatorGetPodcast, deletePodcast);
+
 
 module.exports = router;
