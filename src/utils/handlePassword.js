efd0cfc233389db3904e32e859fdca0d980e3ebe
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-06-27 23:52:09 -0400

    Creación de HandlePassword

---
+++ src/utils/handlePassword.js
@@ -0,0 +1,11 @@
+const bcrypt = require('bcryptjs');
+
+const encryptPassword = async(password) => {
+
+}
+
+const comparePassword = async(password, passwordDB) => {
+
+}
+
+module.exports = { encryptPassword, comparePassword };
