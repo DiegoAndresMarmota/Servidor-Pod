a56a569c79d8862003e0a92aacf3d6c2ddf48c86
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-06-27 23:51:48 -0400

    Act. de post /register

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { validatorRegisterUser, validatorLoginUser } = require("../validators/auth");
+const { matchedData } = require("express-validator");
 
 
 /**
@@ -15,6 +16,8 @@ router.post("/login", validatorLoginUser, (req, res) => {
  * Ingresar al podcast a través del register
  */
 router.post("/register", validatorRegisterUser, (req, res) => {
+    req = matchedData(req);
+    res.send({data: req})
 
 });
 
